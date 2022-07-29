641e8170bd174fcdcdc2ffc6f5c3b0a7daca419d
Sbaoor <2959435045@qq.com>
2022-07-29 21:02:13 +0800

    maybe fix it

--- download.sh
+++ download.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 echo "https://void.bedrock.dev/resources/$1.zip"
 
-function get_char()
-{
-  SAVEDSTTY=`stty -g`
-  stty -echo
-  stty cbreak
-  dd if=/dev/tty bs=1 count=1 2> /dev/null
-  stty -raw
-  stty echo
-  stty $SAVEDSTTY
-}
+#function get_char()
+# {
+#  SAVEDSTTY=`stty -g`
+#  stty -echo
+#  stty cbreak
+#  dd if=/dev/tty bs=1 count=1 2> /dev/null
+#  stty -raw
+#  stty echo
+#  stty $SAVEDSTTY
+#}
 
-enable_pause=1
+# enable_pause=1
 
-function pause()
-{
-  if [ "x$1" != "x" ]; then
-    echo $1
-  fi
-  if [ $enable_pause -eq 1 ]; then
-    echo "Press any key to continue!"
-    char=`get_char`
-  fi
-}
+#function pause()
+# {
+#  if [ "x$1" != "x" ]; then
+#    echo $1
+#  fi
+#  if [ $enable_pause -eq 1 ]; then
+#    echo "Press any key to continue!"
+#    char=`get_char`
+#  fi
+#}
 
 curl https://void.bedrock.dev/resources/$1.zip --output ./tmp/$1.zip
 
