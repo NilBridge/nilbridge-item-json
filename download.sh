da3bf28a79c4f7efd2c7623232d38758f5cbb9e6
Sbaoor <2959435045@qq.com>
2022-07-29 20:59:02 +0800

    big fix!!!

--- download.sh
+++ download.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
-echo $1
+echo "https://void.bedrock.dev/resources/$1.zip"
 
-if [ ! -d "/web" ]; then
-  mkdir /web
-fi
-if [ ! -d "/tmp" ]; then
-  mkdir /tmp
-fi
-if [ ! -d "/resources" ]; then
-  mkdir /resources
-fi
+function get_char()
+{
+  SAVEDSTTY=`stty -g`
+  stty -echo
+  stty cbreak
+  dd if=/dev/tty bs=1 count=1 2> /dev/null
+  stty -raw
+  stty echo
+  stty $SAVEDSTTY
+}
 
-set version=$1
+enable_pause=1
+
+function pause()
+{
+  if [ "x$1" != "x" ]; then
+    echo $1
+  fi
+  if [ $enable_pause -eq 1 ]; then
+    echo "Press any key to continue!"
+    char=`get_char`
+  fi
+}
 
 curl https://void.bedrock.dev/resources/$1.zip --output ./tmp/$1.zip
 
-tar -xf ./tmp/$1.zip -C ./resources/
+#tar xvf ./tmp/$1.zip -C ./resources/
 
+unzip -d ./resources/ ./tmp/$1.zip
+
+#pause "ok"
